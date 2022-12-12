3f051cdb65e127f5d31ab15a5d4a0bca6c97b7de
EBNoir <100785117+EBNoir@users.noreply.github.com>
2022-12-12 17:24:37 -0300

    Update App.js

--- proyectoEvento/src/App.js
+++ proyectoEvento/src/App.js
@@ -20,7 +20,7 @@ function App(){
     return(
         <div className='container'>
             <div className='row'>{
-                data.GET_EVENTOS.map(({titulo, descripcion, imagen})=>{
+                data.getEventos.map(({titulo, descripcion, imagen})=>{
                     return (
                         <div className='col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2'>
                             <div className='card'>
@@ -28,7 +28,7 @@ function App(){
                                     <h4 className='card-title'>{titulo}</h4>
                                     <p className='card-text'>{descripcion}</p>
                                     <img className='card-img-bottom w-100'
-                                    src = {process.env.PUBLIC_URL + "...\static\images" + imagen}
+                                    src = {process.env.PUBLIC_URL + ".../static/images/" + imagen}
                                     alt = "Card image"/>
                                 </div>
                             </div>
